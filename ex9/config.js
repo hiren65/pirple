bed519007daf81943d660f10e4c31974e7fbe0ba
hirenkumar <hirenkumar@me.com>
2018-12-22 13:02:17 +1100

    config.js changed for http and https same for index.js

--- ex9/config.js
+++ ex9/config.js
@@ -8,13 +8,15 @@
 
  //Stagging (default) enviroment
  environments.staging = {
-   'port' : 3000,
+   'httpPort' : 3000,
+   'httpsPort' : 3001,
    'envName' : 'staging'
  };
 
  //Production enviroment
  environments.production = {
-   'port' : 5000,
+   'httpPort' : 5000,
+   'httpsPort' : 5001,
    'envName' : 'production'
  };
 //Determine wich enviroment was passed as a command line argument
